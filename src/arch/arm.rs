e4b8a9dbf075952b63fb390ef4a96fe3f4b87870
edef <edef@edef.eu>
2016-08-22 15:22:10 -0400

    add ARM support

---
+++ src/arch/arm.rs
@@ -0,0 +1,16 @@
+pub type Value = u32;
+
+#[inline(always)]
+pub unsafe fn do_client_request(default: Value, args: &[Value; 6]) -> Value {
+  let result;
+
+  asm!("mov r12, r12, ror #3  ; mov r12, r12, ror #13
+        mov r12, r12, ror #29 ; mov r12, r12, ror #19
+        orr r10, r10, r10"
+      : "={r3}" (result)
+      : "{r4}" (args.as_ptr())
+        "{r3}" (default)
+      : "cc", "memory"
+      : "volatile");
+  result
+}
